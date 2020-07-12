a9398bb4b75c2f89793dbdb436fff1376b7e2b8d
tusqasi <2002.tushar.kuntawar@gmail.com>
2020-07-12 16:40:44 +0530

    ➕Added GUI Basic

---
+++ gui-working.py
@@ -0,0 +1,78 @@
+from tkinter import * 
+
+
+
+
+
+class AppWin:
+	def __init__(self,master):
+
+		#frames defined or whatever
+
+		addFrame        = Frame(master)          #packed
+		chgFrame    = Frame(master)			 #packed
+		lastFrame       = Frame(master)			 #packed
+		#frames packed
+		
+		addFrame.grid(row=0, column=0)           
+		chgFrame.grid(row=1,column=0)        
+		lastFrame.grid(row=3)                    
+
+		#labels
+		addNameLabel    = Label(addFrame,		 #packed
+							    text='Task Name',
+							    anchor="e",
+							    width=14)
+		addDurLabel     = Label(addFrame, 		 #packed
+							    text='Task duration',
+							    anchor="e",
+							    width=14)
+		addDescLabel    = Label(addFrame,		 #packed
+							    text='Task Description',
+							    anchor="e",
+							    width=14)
+
+		chgNameLabel    = Label(chgFrame,
+								text='New Name',
+								anchor='e',
+								width=16)
+
+		chgDurLabel     = Label(chgFrame,
+					 		    text='New Duration',
+					 		    anchor='e',
+					 		    width=16)
+
+		chgDescLabel    = Label(chgFrame,
+								text='New Description',
+								anchor='e',
+								width=16)
+		#labels packed
+		addDurLabel.grid(row=1, column=0)
+		addNameLabel.grid(row=0, column=0)
+		addDescLabel.grid(row=2, column=0)
+
+		chgNameLabel.grid(row=0, column=0)
+		chgDurLabel.grid(row=1, column=0)
+		chgDescLabel.grid(row=2, column=0)
+
+
+		#entry boxes 
+		self.addNameEntry    = Entry(addFrame)        
+		self.addDurEntry     = Entry(addFrame)
+		self.addDescEntry    = Entry(addFrame)
+
+		self.addNameEntry.grid(row=0,column=1)
+		self.addDurEntry.grid(row=1,column=1) 
+		self.addDescEntry.grid(row=2,column=1)
+		
+ 		 
+
+	def DoThings(e):
+		print("YeYa")
+
+root = Tk()
+#root.geometry("800x600")
+
+app = AppWin(root)
+
+root.mainloop()
