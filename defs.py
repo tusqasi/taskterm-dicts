5b93e490d0583a6b314b89ac20b1468cbc9eb5e5
tusqasi <2002.tushar.kuntawar@gmail.com>
2020-07-17 21:03:52 +0530

    🤦 adding defs.py

---
+++ defs.py
@@ -0,0 +1,10 @@
+
+class TaskFuncs:
+	"""docstring for TaskFuncs"""
+			
+	def addTask(ind, name , dur, desc):
+		return {'index':ind,
+				'name':name,
+				'duration':dur,
+				'description':desc }
+				
